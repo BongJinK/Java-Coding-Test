3da0644bebfb9d1164acf46680abb207131c459b
BongJinK <121011378+BongJinK@users.noreply.github.com>
2024-06-27 23:49:11 +0900

    스택

--- baekjoon/src/BaekJoon/week1/Sol_2_12605.java
+++ baekjoon/src/BaekJoon/week1/Sol_2_12605.java
@@ -2,7 +2,7 @@ package BaekJoon.week1;
 
 import java.util.*;
 
-public class Sol_12605 {
+public class Sol_2_12605 {
     /**
      * 스페이스로 띄어쓰기 된 단어들의 리스트가 주어질때, 단어들을 반대 순서로 뒤집어라.
      * 각 라인은 w개의 영단어로 이루어져 있으며, 총 L개의 알파벳을 가진다.
